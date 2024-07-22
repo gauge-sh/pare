88a72d53bd9bb764440331c1e9e817c93cf79c00
Evan Doyle <evanmdoyle@gmail.com>
2024-07-22 12:02:44 -0700

    First pass implementation of python lambda deploys

--- api/src/deploy/routes.py
+++ api/src/deploy/routes.py
@@ -3,13 +3,17 @@ from fastapi import APIRouter, UploadFile
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
-from src.build.zip import write_to_zipfile
+from src.build.python_lambda import install_deps_to_dir
+from src.deploy.lambda_deploy import deploy_python_lambda_function
+from src.transform import build_lambda_handler
+from src.build.zip import write_extended_zipfile, write_to_zipfile
 from src.constants import API_VERSION
 
 router = APIRouter(prefix=f"/{API_VERSION}")
 
 
 class DeploymentConfig(BaseModel):
+    name: str
     path: str
     python_version: str
     requirements: list[str] = Field(default_factory=list)
@@ -26,12 +30,17 @@ async def deploy_zip(file: UploadFile, deployments: list[DeploymentConfig]):
         except ValueError as err:
             return JSONResponse(status_code=400, content={"error": str(err)})
         for deployment in deployments:
-            # create subfolder
-            # install requirements into subfolder
-            # build lambda handler from path
-            # create new zip from uploaded zip, lambda handler, and requirements
-            # use boto3 to upload this zip to a lambda with a derived name (upsert)
-            ...
-        
-        # after all deployments done, update record in RDS
-        return {"status": "WIP"}
+            # TODO: validate deployment name
+            build_path = tmp_dir / deployment.name
+            build_lambda_handler(symbol_path=deployment.path, output_path=build_path / "lambda_function.py")
+            install_deps_to_dir(dependencies=deployment.requirements, python_version=deployment.python_version, output_dir=build_path)
+            deployment_package_path = tmp_dir / f"{deployment.name}.zip"
+            write_extended_zipfile(existing_zipfile=zipfile_path, additional_paths=[build_path], output_path=deployment_package_path)
+
+            deploy_python_lambda_function(
+                function_name=deployment.name,
+                zip_file=deployment_package_path,
+                python_version=deployment.python_version
+            )
+            # TODO: after each deployment is done, update record in RDS
+        return {"status": "OK"}
--- api/src/settings.py
+++ api/src/settings.py
@@ -9,3 +9,6 @@ DB_PASSWORD = env.str("DB_PASSWORD")
 DB_HOST = env.str("DB_HOST", "localhost")
 DB_PORT = env.int("DB_PORT", 5432)
 DB_NAME = env.str("DB_NAME", "postgres")
+
+
+LAMBDA_ROLE_ARN = env.str("LAMBDA_ROLE_ARN")
