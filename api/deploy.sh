c8cdaa8bb3989302ef8a5c74787e0f95f3900afb
Evan Doyle <evanmdoyle@gmail.com>
2024-07-23 16:50:04 -0700

    uv for pip compile in make deploy

--- api/deploy.sh
+++ api/deploy.sh
@@ -24,7 +24,7 @@ fi
 
 source $VENV_DIR/bin/activate
 pip install --upgrade pip pip-tools uv
-pip-compile $API_DIR/requirements.in -o $API_DIR/requirements.txt
+uv pip compile $API_DIR/requirements.in -o $API_DIR/requirements.txt
 uv pip install -r $API_DIR/requirements.txt
 
 cd $API_DIR
