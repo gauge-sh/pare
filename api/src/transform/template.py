7bd57348cf280005a3a8754d8615e2bc66649dec
Evan Doyle <evanmdoyle@gmail.com>
2024-07-22 22:05:51 -0700

    Fix scoping issue with exception when handler fails to start

--- api/src/transform/template.py
+++ api/src/transform/template.py
@@ -3,9 +3,10 @@ from ${mod_path} import ${target_symbol} as lambda_function
 try:
     lambda_handler = lambda_function.as_lambda_function_url_handler()
 except Exception as e:
+    exception_msg = str(e)
     def lambda_handler(evt, ctx):
         full_path = "${mod_path}" + ":" + "${target_symbol}"
         return {
             "statusCode": 500,
-            "body": f"Lambda for '{full_path}' failed to start: {e}"
+            "body": f"Lambda for '{full_path}' failed to start: {exception_msg}"
         }
