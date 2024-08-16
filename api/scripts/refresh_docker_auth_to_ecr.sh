042c0ce1ad4b62d9f412889f2253af8d7433e76b
Evan Doyle <evanmdoyle@gmail.com>
2024-08-16 10:31:50 -0700

    Temporary fix for venv in auth refresh

--- api/scripts/refresh_docker_auth_to_ecr.sh
+++ api/scripts/refresh_docker_auth_to_ecr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-source /home/ec2-user/pare-scripts/.venv/bin/activate
+source /home/ec2-user/gauge-serverless/.venv/bin/activate
 python /home/ec2-user/pare-scripts/refresh_docker_auth_to_ecr.py
