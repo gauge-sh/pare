ea4126a6e23d5bf945ddd8acde76968d2e093f47
Evan Doyle <evanmdoyle@gmail.com>
2024-08-16 10:59:54 -0700

    Explicitly use bash

--- api/src/utils.py
+++ api/src/utils.py
@@ -16,6 +16,7 @@ async def run_async_subprocess(command: str):
     process = subprocess.Popen(
         command,
         shell=True,
+        executable="/bin/bash",
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,  # This makes the output strings instead of bytes
